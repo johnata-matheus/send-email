13f4b9c624c29139dca42fe62c04244d6e52da7b
Johnata Matheus <johnata@paytour.com.br>
2024-08-14 23:29:31 -0300

    fix: adjust emailDto

--- src/main/java/br/com/email/dtos/EmailDto.java
+++ src/main/java/br/com/email/dtos/EmailDto.java
@@ -1,20 +1,62 @@
 package br.com.email.dtos;
 
+import br.com.email.models.EmailModel;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class EmailDto {
-  
+public class EmailDto{
   @NotBlank
-  private String ownerRef;
-  @NotBlank
-  @Email
-  private String emailFrom;
-  @NotBlank
-  private String subject;
-  @NotBlank
-  private String text;
+    private String ownerRef;
 
+    @NotBlank
+    @Email
+    private String emailFrom;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String text;
+
+    public EmailDto() {
+    }
+
+    public EmailDto(String ownerRef, String emailFrom, String subject, String text) {
+        this.ownerRef = ownerRef;
+        this.emailFrom = emailFrom;
+        this.subject = subject;
+        this.text = text;
+    }
+
+    public String getOwnerRef() {
+        return ownerRef;
+    }
+
+    public void setOwnerRef(String ownerRef) {
+        this.ownerRef = ownerRef;
+    }
+
+    public String getEmailFrom() {
+        return emailFrom;
+    }
+
+    public void setEmailFrom(String emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
